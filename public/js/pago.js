7e17523146ff18a99d604a3e33d95cd678c9f02c
valeriavalles <valeria.sv@outlook.es>
2018-03-31 17:30:20 -0500

    actulizando cambios

--- public/js/pago.js
+++ public/js/pago.js
@@ -1,10 +1,48 @@
-
-
-$(function() {   
-  $('.slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
+$(document).ready(function() {
+  // SLIDER 
+  $(function() {   
+    $('.slider').slick({
+      slidesToShow: 0,
+      slidesToScroll: 0,
+      autoplay: false,
+    // autoplaySpeed: 2000,
+    });
   });
+
+
+  const dataLogo = [
+    {
+      url: 'https://btpucp.pucp.edu.pe/wp-content/uploads/2012/07/logo-oficial-412x120.png',
+      banco: 'bcp'
+    },
+    {
+      url: 'https://eisaf.es/wp-content/uploads/2018/03/bbva_foro-500x150.png',
+      banco: 'bbva'
+    },
+    {
+      url: 'https://btpucp.pucp.edu.pe/wp-content/uploads/2012/07/logo-oficial-412x120.png',
+      banco: 'bcp'
+    },
+    {
+      url: 'https://eisaf.es/wp-content/uploads/2018/03/bbva_foro-500x150.png',
+      banco: 'bbva'
+    }
+  ];
+  const datos = `
+  <div class="item sli-item" >
+      <img src=${dataLogo[0].url}  />
+      <p>${dataLogo[0].banco}</p>
+  </div>
+`;
+
+  function imagenes(dataLogo) {
+  
+    
+    const contPadre = $('.slider');
+    contPadre.append(datos);
+  }
+  imagenes();
+  console.log(dataLogo);
+/* et show = dataLogo[0].url;
+console.log(show);*/
 });
