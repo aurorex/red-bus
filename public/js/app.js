ef72c157fd784e9f5c5a2fcc68c0651780e3d842
darcycervh <darcycervera@gmail.com>
2018-04-01 03:25:19 -0500

    intentando añadir carrusel

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,14 @@
 $('#myTab a').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-  })
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    e.target // newly activated tab
-    e.relatedTarget // previous active tab
-  })
+  e.preventDefault()
+  $(this).tab('show')
+})
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  e.target // newly activated tab
+  e.relatedTarget // previous active tab
+})
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
